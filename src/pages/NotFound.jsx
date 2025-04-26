f8d4a8b258b4f8b03fea9a995d04e89e29ec7dfd
Manuel <luisparedescuenca@gmail.com>
2025-04-25 22:47:05 -0300

    Not Found Page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import LOGO from '../assets/logo.png';
+
 
 const NotFound = () => {
     return (
-        <div className='mt-5 d-flex justify-content-center align-items-center bg-secondary height-25'>
-            <h1 className='text-danger ff-anton'>Error 404&nbsp; : &nbsp;</h1>
-            <h2 className='fw-bold ff-anton'>Ruta no encontrada</h2>
+        <div className="container text-center mt-3">
+            <img className='w-25' src={LOGO} alt="" />
+            <h1 className="display-1 fw-bold">404</h1>
+            <p className="fs-3">Oops! Page not found</p>
+            <p className="lead mb-4">
+                The page you're looking for doesn't exist or has been moved.
+            </p>
+            <Link to="/" className="btn btn-outline-dark">
+                Go Back Home
+            </Link>
         </div>
     )
 }
