2ee68bd3672bdc79b8a262d4bf1b135707fb60ab
Manuel <luisparedescuenca@gmail.com>
2025-04-25 22:42:24 -0300

    Main Screen

--- src/pages/MainScreen.jsx
+++ src/pages/MainScreen.jsx
@@ -1,11 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import BACKGROUND from '../assets/background.png';
 
 
 const MainScreen = () => {
     return (
-        <>
-            <h1>Main</h1>
-        </>
-    )
-}
+        <div className="container my-3 text-center">
+            <img className='mb-3' src={BACKGROUND} alt="" />
 
-export default MainScreen
+            <h1 className="mb-4">Welcome to the Rick and Morty Universe</h1>
+            <p className="lead mb-5">
+            This app lets you explore characters, episodes, and locations from the Rick and Morty universe.
+            <br /> 
+            All data is powered by the <a href="https://rickandmortyapi.com/" target="_blank" rel="noopener noreferrer">Rick and Morty API</a>
+            </p>
+
+            <div className="row justify-content-center gap-4">
+                <Link to="/characters/1" className="btn btn-outline-dark col-10 col-md-3">
+                    Browse Characters
+                </Link>
+                <Link to="/locations/1" className="btn btn-outline-dark col-10 col-md-3">
+                    Browse Locations
+                </Link>
+                <Link to="/episodes/1" className="btn btn-outline-dark col-10 col-md-3">
+                    Browse Episodes
+                </Link>
+            </div>
+        </div>
+    );
+};
+
+export default MainScreen;
